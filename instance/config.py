3396be96a34ab4907a7222bba5fb12709ecdbeb4
Matthieu Amoros <matthieu.amoros@andinexia.com>
2021-03-23 17:43:10 -0300

    Se borró ejemplo de configuración y se remplazó por un template de configuración

--- instance/config.py
+++
@@ -1,8 +0,0 @@
-FLASK_LISTEN_PORT = "5001"
-FLASK_ENDPOINT = "127.0.0.1"
-DEBUG_MODE = True
-
-APPLICATION_NAME = "TEST"
-REFERER = "https://test.cl"
-
-WKHTMLTOPDF_EXE_PATH = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
