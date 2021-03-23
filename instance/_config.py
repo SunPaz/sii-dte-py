b205312319e70c8c76f67ebf6823eef57ea60286
Matthieu Amoros <matthieu.amoros@andinexia.com>
2021-03-23 17:27:04 -0300

    Se actualizó el archivo ignore para no tomar en cuenta los datos de configuración

---
+++ instance/_config.py
@@ -0,0 +1,14 @@
+""" Template file, to be renamed to config.py """
+
+FLASK_LISTEN_PORT = "5001"
+FLASK_ENDPOINT = "127.0.0.1"
+DEBUG_MODE = True
+
+APPLICATION_NAME = "TEST"
+REFERER = "https://test.cl"
+
+WKHTMLTOPDF_EXE_PATH = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
+SII_INBOX_EMAIL_SERVER = ""
+SII_INBOX_EMAIL_ACCOUNT = ""
+SII_INBOX_EMAIL_PASSWORD = ""
+JSON_LAST_SEEN_PATH = "last_seen.json"
